ab557c2fef4103159681d4a9ed8913bfa1cf828c
amiel <amielnoy@gmail.com>
2023-05-09 16:25:19 +0300

    fix remove token param call use default token

--- tests/ApiTests/ApiSecurity/PayPalApiTests/PayPalTests.py
+++ tests/ApiTests/ApiSecurity/PayPalApiTests/PayPalTests.py
@@ -6,6 +6,6 @@ from Utils.HttpRequests.base_http_requests import BaseHttpRequests
 
 class TestPayPalSecurityTests:
     def test_user_info(self):
-        headers = BaseHttpRequests.get_headers(token=token_value)
+        headers = BaseHttpRequests.get_headers()
         response = PayPalRequests.http_get_request('/v1/identity/oauth2/userinfo', '?schema=paypalv1.1')
         print(response)
