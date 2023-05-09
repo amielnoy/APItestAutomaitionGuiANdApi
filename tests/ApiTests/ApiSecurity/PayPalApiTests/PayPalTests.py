317947257d803ed4800bdf9183569698737acb54
amiel <amielnoy@gmail.com>
2023-05-09 16:35:33 +0300

    use paypal derived class token var

--- tests/ApiTests/ApiSecurity/PayPalApiTests/PayPalTests.py
+++ tests/ApiTests/ApiSecurity/PayPalApiTests/PayPalTests.py
@@ -2,10 +2,8 @@ from Utils.HttpRequests.PayPalApi.PayPalApiHttpRequests import PayPalRequests
 from Utils.HttpRequests.base_http_requests import BaseHttpRequests
 
 
-# {{base_url}}/v1/identity/oauth2/userinfo?schema=paypalv1.1
-
 class TestPayPalSecurityTests:
     def test_user_info(self):
-        headers = BaseHttpRequests.get_headers()
+        headers = BaseHttpRequests.get_headers(PayPalRequests.token_value)
         response = PayPalRequests.http_get_request('/v1/identity/oauth2/userinfo', '?schema=paypalv1.1')
         print(response)
